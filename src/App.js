cf97eee2732959d914856496aa0becd362e86b49
Subhash Shrestha <subhashshrestha3@gmail.com>
2021-05-17 19:45:27 +0930

    added history

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { createBrowserHistory } from "history";
 
+const history = createBrowserHistory();
 
 function App() {
   return (
